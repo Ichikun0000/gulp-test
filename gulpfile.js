030d0fe93f209bf52b11781ac0522b3a00afc223
Ichikun0000 <kooooooichi09@gmail.com>
2021-03-28 08:39:16 +0900

    ベンダープレフィックス自動付与 メディアクエリをまとめる 画像の自動圧縮

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 const gulp = require('gulp');
+const del = require('del');
 
 //scss
-const sass = require('gulp-dart-sass');//Dart Sass はSass公式が推奨 @use構文などが使える
-const plumber = require("gulp-plumber");    // エラーが発生しても強制終了させない
-const notify = require("gulp-notify");      // エラー発生時のアラート出力
-const browserSync = require("browser-sync");      //ブラウザリロード
+const sass = require('gulp-dart-sass'); //Dart Sass はSass公式が推奨 @use構文などが使える
+const plumber = require("gulp-plumber"); // エラーが発生しても強制終了させない
+const notify = require("gulp-notify"); // エラー発生時のアラート出力
+const browserSync = require("browser-sync"); //ブラウザリロード
+const autoprefixer = require('gulp-autoprefixer'); //ベンダープレフィックス自動付与
+const postcss = require("gulp-postcss"); //css-mqpackerを使うために必要
+const mqpacker = require('css-mqpacker'); //メディアクエリをまとめる
+
+
+//画像圧縮
+const imagemin = require("gulp-imagemin");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const imageminPngquant = require("imagemin-pngquant");
+const imageminSvgo = require("imagemin-svgo");
 
 
 // 入出力するフォルダを指定
@@ -14,13 +25,29 @@ const distBase = './_static/dist';
 
 const srcPath = {
   'scss': srcBase + '/scss/**/*.scss',
-  'html': srcBase + '/**/*.html'
+  'html': srcBase + '/**/*.html',
+  'img': srcBase + '/img/**/*'
 };
 
 const distPath = {
   'css': distBase + '/css/',
-  'html': distBase + '/'
+  'html': distBase + '/',
+  'img': distBase + '/img/'
 };
+
+
+/**
+ * clean
+ */
+const clean = () => {
+  return del(distBase + '/**');
+}
+
+//ベンダープレフィックスを付与する条件
+const TARGET_BROWSERS = [
+  'last 2 versions', //各ブラウザの2世代前までのバージョンを担保
+  'ie >= 11' //IE11を担保
+];
 
 /**
  * sass
@@ -29,20 +56,51 @@ const distPath = {
 
 const cssSass = () => {
   return gulp.src(srcPath.scss, {
-    sourcemaps: true
-  })
+      sourcemaps: true
+    })
     .pipe(
       //エラーが出ても処理を止めない
       plumber({
         errorHandler: notify.onError('Error:<%= error.message %>')
       }))
-    .pipe(sass({ outputStyle: 'compressed' })) //指定できるキー expanded compact compressed
-    .pipe(gulp.dest(distPath.css, { sourcemaps: './' }))         //コンパイル先
+    .pipe(sass({
+      outputStyle: 'expanded'
+    })) //指定できるキー expanded compressed
+    .pipe(autoprefixer(TARGET_BROWSERS))
+    .pipe(postcss([mqpacker()])) // メディアクエリをまとめる
+    .pipe(gulp.dest(distPath.css, {
+      sourcemaps: './'
+    })) //コンパイル先
     .pipe(browserSync.stream())
     .pipe(notify({
       message: 'Sassをコンパイルしました！',
       onLast: true
     }))
+}
+
+/**
+ * 画像圧縮
+ */
+const imgImagemin = () => {
+  return gulp.src(srcPath.img)
+    .pipe(
+      imagemin(
+        [
+          imageminMozjpeg({
+            quality: 80
+          }),
+          imageminPngquant(),
+          imageminSvgo({
+            plugins: [{
+              removeViewbox: false
+            }]
+          })
+        ], {
+          verbose: true
+        }
+      )
+    )
+    .pipe(gulp.dest(distPath.img))
 }
 
 
@@ -82,6 +140,7 @@ const browserSyncReload = (done) => {
 const watchFiles = () => {
   gulp.watch(srcPath.scss, gulp.series(cssSass))
   gulp.watch(srcPath.html, gulp.series(html, browserSyncReload))
+  gulp.watch(srcPath.img, gulp.series(imgImagemin, browserSyncReload))
 }
 
 /**
@@ -91,6 +150,7 @@ const watchFiles = () => {
  * 一度cleanでdistフォルダ内を削除し、最新のものをdistする
  */
 exports.default = gulp.series(
-  gulp.parallel(html, cssSass),
+  clean,
+  gulp.parallel(html, cssSass, imgImagemin),
   gulp.parallel(watchFiles, browserSyncFunc)
 );
